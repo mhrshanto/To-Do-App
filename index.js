eb998851f46fad43c360f1d1e2fd4068dfdb0675
Md Habibur Rahman Shanto <mhrshanto36@gmail.com>
2023-05-26 21:14:55 +0600

    Some updated

--- index.js
+++ index.js
@@ -14,5 +14,56 @@ let createTask = (task) => {
 
     label.innerText = task;
     checkBox.type = 'checkbox';
-    listItem.appendChild();
+    listItem.appendChild(checkBox);
+    listItem.appendChild(label);
+
+    return listItem;
 }
+
+
+let addTask = (event) => {
+    event.preventDefault();
+    let listItem = createTask(newTask.value);
+
+    ictList.appendChild(listItem);
+    newTask.value = "";
+    bindIncompleteItems(listItem, completeTask);
+}
+
+let completeTask = () => {
+    let listItem = this.parentNode;
+    let deleteBtn = document.createElement('button');
+    deleteBtn.innerText = 'Delete';
+    deleteBtn.className = 'delete';
+
+    listItem.appendChild(deleteBtn);
+
+
+    let checkBox = listItem.querySelector('input[type="checkbox"]');
+
+    checkBox.remove();
+
+
+    ctList.appendChild(listItem);
+
+    bindCompleteItems(listItem, deleteTask);
+}
+
+let deleteTask = () => {
+    let listItem = this.parentNode();
+    let ul = listItem.parentNode();
+    ul.removeChild(listItem);
+}
+
+let bindIncompleteItems = (taskItem, checkBoxClick) => {
+    let checkBox = taskItem.querySelector('input[type="checkbox"]');
+    checkBox.onchange = checkBoxClick;
+}
+
+let bindCompleteItems = (taskItem, deleteButtonClick) => {
+    let deleteButton = taskItem.querySelector('.delete')
+    deleteButton.onclick = deleteButtonClick;
+}
+
+
+taskForm.addEventListener('submit', addTask);
