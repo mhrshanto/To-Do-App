15d2d3d695dba11747aaee9735ba686ecd97a18a
Md Habibur Rahman Shanto <mhrshanto36@gmail.com>
2023-05-26 20:21:18 +0600

    Some code added

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-const newTask = document.querySelector('#add-task');
+let newTask = document.querySelector('#add-task');
+let taskForm = document.querySelector('form');
+
+let ictList = document.querySelector('#ictList');
+let ctList = document.querySelector('#ctList');
+
+
+
+// Functions
+let createTask = (task) => {
+    let listItem = document.createElement('li');
+    let checkBox = document.createElement('input');
+    let label = document.createElement('label');
+
+    label.innerText = task;
+    checkBox.type = 'checkbox';
+    listItem.appendChild();
+}
